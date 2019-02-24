30b84befe0123cde1fc55c4de25c634bbf8d25d7
Krishnaarunangsu <arunangsutech@gmail.com>
2019-02-24 21:25:08 +0530

    Initial Commit

---
+++ src/main/java/com/git/ci/Employee.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package com.git.ci;
+
+import java.io.Serializable;
+
+/**
+ * @author Arunangsu
+ *
+ */
+public class Employee implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4645561467970099412L;
+	private String employeeId;
+	private String employeeName;
+
+	/**
+	 * 
+	 */
+	public Employee() {
+		// No-argument Constructor
+	}
+
+	/**
+	 * @return the employeeId
+	 */
+	public String getEmployeeId() {
+		return employeeId;
+	}
+
+	/**
+	 * @param employeeId the employeeId to set
+	 */
+	public void setEmployeeId(String employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	/**
+	 * @return the employeeName
+	 */
+	public String getEmployeeName() {
+		return employeeName;
+	}
+
+	/**
+	 * @param employeeName the employeeName to set
+	 */
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+
+	/**
+	 * @param employeeId
+	 * @param employeeName
+	 */
+	public Employee(String employeeId, String employeeName) {
+		super();
+		this.employeeId = employeeId;
+		this.employeeName = employeeName;
+	}
+	
+	
+
+}
